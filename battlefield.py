4a7e245ef1e81337248104096d32d4a1d7401fb3
Jonathan Marshall <jonkylemars@yahoo.com>
2022-05-16 09:11:53 -0500

    Final Update

--- battlefield.py
+++ battlefield.py
@@ -10,27 +10,32 @@ class Battlefield:
     def run_game(self):
         self.display_welcome()
         self.battle_phase()
+        self.display_winner()
 
     def display_welcome(self):
         print('Welcome to the battle between Time an d Technology!\nThere can only be one Victor!')
 
     def battle_phase(self):
         
-        while self.robot.health >= 0:
+        while self.robot.health >= 0 and self.dinosaur.health >= 0:
             
             self.dinosaur.attack_robot(self.robot)
             print(f'Indominus attacked Bruticus.\n Bruticus health is now: {self.robot.health}')
             
             self.robot.attack_dinosaur(self.dinosaur)
             print(f'Bruticus attacked Indominus.\n Indominus health is now: {self.dinosaur.health}')
+
+            if self.robot.health == 0  or self.dinosaur.health <= 40:
+                break
             
-            self.display_winner()
-                    
+                
+                  
 
     def display_winner(self):
             
-            if self.dinosaur.health >= 0:
-                print(f' ***{self.dinosaur.name} is the Victor!***')
+                
+            if  self.dinosaur.health >= 0:
+                    print(f' ***{self.dinosaur.name} is the Victor!***')
             
             elif self.robot.health == 0:
-                print(f' ***{self.robot.name} is the Victor!***')
+                    print(f' ***{self.robot.name} is the Victor!***')
